a2945d4d1ef390e165c71589c26bdbe455578879
Christoffarrel28 <valentziuschristoffarrel@gmail.com>
2026-01-10 04:19:28 +0700

    Update streamlit_tsp.py

--- program_training_streamlit/streamlit_tsp.py
+++ program_training_streamlit/streamlit_tsp.py
@@ -1,18 +1,4 @@
 import streamlit as st
-import folium
-from streamlit_folium import st_folium
 
-st.title("TEST FOLIUM")
-
-m = folium.Map(
-    location=[-7.98, 112.63],
-    zoom_start=12,
-    tiles="OpenStreetMap"
-)
-
-folium.Marker(
-    [-7.98, 112.63],
-    popup="Test Marker"
-).add_to(m)
-
-st_folium(m, width=700, height=500)
+st.title("TEST STREAMLIT")
+st.write("Kalau ini muncul, Streamlit kamu AMAN")
