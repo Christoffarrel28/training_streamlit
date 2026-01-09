184364678d91960f68dffbb21ec3461ccc9fd99e
Christoffarrel28 <valentziuschristoffarrel@gmail.com>
2026-01-10 04:13:26 +0700

    Update streamlit_tsp.py

--- program_training_streamlit/streamlit_tsp.py
+++ program_training_streamlit/streamlit_tsp.py
@@ -1,15 +1,16 @@
+import streamlit as st
 import folium
 from streamlit_folium import st_folium
 
 m = folium.Map(
-    location=[-7.97, 112.62],
-    zoom_start=12,
+    location=[-7.98, 112.63],
+    zoom_start=14,
     tiles="OpenStreetMap"
 )
 
 folium.Marker(
-    [-7.97, 112.62],
-    popup="Test Marker"
+    [-7.98, 112.63],
+    popup="Test"
 ).add_to(m)
 
 st_folium(m)
